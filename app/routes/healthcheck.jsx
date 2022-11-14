5db7e0cf803ed9252d12ab90e92a0310bb44657d
Stefan Penner <stefan.penner@gmail.com>
2022-11-13 18:01:07 -0700

    More healthchecks

--- app/routes/healthcheck.jsx
+++ app/routes/healthcheck.jsx
@@ -1,4 +1,14 @@
-export async function loader() {
-  console.log("[healthcheck]");
-  return new Response("OK");
+export async function loader({ request }) {
+  console.log("[healthcheck]", request.headers.get('user-agent'));
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+
+  const url = new URL("/", `http://${host}`);
+  const { ok } = await fetch(url.toString(), { method: "HEAD" });
+  if (ok) {
+    return new Response("OK");
+  } else {
+    console.log("healthcheck ❌", { error });
+    return new Response("ERROR", { status: 500 });
+  }
 }
